64f21fa721437e22d7cbadbbfdbe8e71e7939ccb
Hasan Ahmed Siddiqui <Hasan.Siddiqui7777@gmail.com>
2020-08-03 17:54:59 -0700

    Just a weak body of how the input and output will work

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class Main {
 
-    public static void main(String[] args) {
-        //Just testing the push
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws  Exception{
+        /*
+         * TODO: Now that I've figured out what I'm using for input and outputs of the image I need to make a loop
+         *  where I get the rgb values. Stop when I reach a rgb value that isn't in range, calculate the average of
+         *  the current values and update everything upto where I have stopped to be the average color. Then I do that
+         *  till I reach the last pixel! :)
+         *
+         * TODO: Bring that horrible horrible runtime down somehow. Use them CPSC 320 skills
+         */
+        File in = new File("D:\\IntelliJ_Projects\\MinimalistPainter\\src\\image.png");
+        File out = new File("D:\\IntelliJ_Projects\\MinimalistPainter\\src\\output.png");
+        BufferedImage img = ImageIO.read(in);
+        ImageIO.write(img, "png", out);
     }
 }
