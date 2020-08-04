d3f5d3889cdd64ad1dc59b1573e21a60ad4098e0
Hasan Ahmed Siddiqui <Hasan.Siddiqui7777@gmail.com>
2020-08-03 17:20:14 -0700

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        //Just testing the push
+        //Just testing the push a
         System.out.println("Hello World!");
     }
 }
