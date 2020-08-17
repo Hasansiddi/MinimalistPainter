e52e70952b6f4e26f2fcd2413e212a2d917f1035
Hasan Ahmed Siddiqui <Hasan.Siddiqui7777@gmail.com>
2020-08-16 17:29:59 -0700

    A more solid skeleton of what's generally supposed to be happening. Doesn't work yet though, I probably need to work with red, green, blue, from the RGB value I get seperately.

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,43 @@ public class Main {
         File in = new File("D:\\IntelliJ_Projects\\MinimalistPainter\\src\\image.png");
         File out = new File("D:\\IntelliJ_Projects\\MinimalistPainter\\src\\output.png");
         BufferedImage img = ImageIO.read(in);
+
+        int width = img.getWidth();
+        int height = img.getHeight();
+        int currPixel = 0;
+        int total  = 0;
+        int count = 0;
+        int prevPixel = 0;
+        int startHeight = 0;
+        int startWidth = 0;
+
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                if(prevPixel > 0 && (currPixel - prevPixel) >= 10){
+                    SetAllPrev(count, total, i, j,startHeight,startWidth, img);
+                    count = 0;
+                    total = 0;
+                    startWidth = i;
+                    startHeight = j;
+                }
+                prevPixel = currPixel;
+                currPixel = img.getRGB(i, j);
+                total = total + currPixel;
+                count++;
+            }
+        }
+
         ImageIO.write(img, "png", out);
+        System.out.println("Task completed!");
+
+    }
+
+    public static void SetAllPrev(int count, int total, int i, int j, int sH, int sW, BufferedImage img){
+        int avg = total/count;
+        for(int x = sW; x < i; i++) {
+            for (int y = sH; y < j; j++) {
+                img.setRGB(x, y, avg);
+            }
+        }
     }
 }
